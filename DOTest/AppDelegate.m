52574e57f8220b06eb7aaa4b37738caca0e5859e
Alexander Dobrynin <alexdobry@pip07.inf.fh-koeln.de>
2013-10-22 13:51:41 +0200

    Initial Commit

---
+++ DOTest/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  DOTest
+//
+//  Created by Alexander Dobrynin on 22.10.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
