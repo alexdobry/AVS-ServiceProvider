85c88d93e0fe4cb516c7b778009d9ab91b778581
Alexander Dobrynin <alexdobry@pip07.inf.fh-koeln.de>
2013-11-12 17:02:09 +0100

    hough transformation mit mehr matches. außerdem werden verdeckte kreise ebenfalls erkannt. einflussfaktor für die HT ist die spanne zwischen min, max und raduis

--- DOTest/AppDelegate.m
+++ DOTest/AppDelegate.m
@@ -24,6 +24,8 @@
 	
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {    
+    NSDate *methodStart = [NSDate date];
+
     // Load Image from local storage
     IplImage *src = cvLoadImage("/Users/alexdobry/Developer/AVS-ServiceProvider/DOTest/Foto.jpg", CV_LOAD_IMAGE_UNCHANGED);
     // output image (dst) of the same size and depth as src.
@@ -37,15 +39,15 @@
     
     // using cvSmooth instead of GaussianBlur to smooth the image, otherwise a lot of false circles may be detected
     // src, dst, smoothType, density width, density height, sigma 1, sigma 2
-    cvSmooth(dst, dst, CV_GAUSSIAN, 3, 3, 0, 0);
+    cvSmooth(dst, dst, CV_GAUSSIAN, 11, 11, 0, 0);
     
     // Write the image to a file with a different name
     //cvSaveImage("/Users/alexdobry/Desktop/Foto3.png", dst, 0);
     
     CvMemStorage* storage = cvCreateMemStorage(0);
     // Finds circles in a grayscale image using the Hough transform.
-    // src, storage that will contain the output sequence of found circles, detection method, example code, example code, example code, example code, min, max
-    CvSeq* result = cvHoughCircles(dst, storage, CV_HOUGH_GRADIENT, 2, dst->height/4, 200, 100, 20, 100);
+    // src, storage that will contain the output sequence of found circles, detection method, example code, example code, example code, example code, min, max      mindist dst->height/4
+    CvSeq* result = cvHoughCircles(dst, storage, CV_HOUGH_GRADIENT, 2,100, 200, 100, 30, 120);
     
     NSLog(@"total: %d", result->total);
     
@@ -53,7 +55,7 @@
         // returns a pointer (float array) to a sequence element according to its index.
         // x = [0], y = [1], r = [2]
         float *detectedCircle = (float*) cvGetSeqElem(result, i);
-        NSLog(@"x = %f, y = %f, r = %f", detectedCircle[0], detectedCircle[1], detectedCircle[2]);
+        //NSLog(@"x = %f, y = %f, r = %f", detectedCircle[0], detectedCircle[1], detectedCircle[2]);
         
         // Draws a circle
         // src, center (combination of x and y), radius, color (red), thickness, example code, example code
@@ -66,6 +68,16 @@
     cvReleaseMemStorage(&storage);
     cvReleaseImage(&dst);
     cvReleaseImage(&src);
+    
+    NSDate *methodFinish = [NSDate date];
+    NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate:methodStart];
+    NSLog(@"executionTime = %f", executionTime * 1000);
+    
+    //Einflussfaktoren auf die Anzahl der Ergebnisse der Hough Transformation
+    //Funktion cvHoguhcircles  
+    //  -> min/max radius --> Spanne, welche Größe die Kreise haben dürfen 
+    //  -> mindist --> Spanne zwischen den Kreismittelpunkten 
+    //Bei münzen mindist = 100 und min 30 und max 120 relativ gutes ergebnis
 }
 
 @end
