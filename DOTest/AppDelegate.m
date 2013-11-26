6758f712bcaeafffcfffd6e9ee36fff7fd4131f9
Alexander Dobrynin <alexdobry@pip07.inf.fh-koeln.de>
2013-11-26 15:01:06 +0100

    merged branch DynamischeInfrastruktur mit opencv_HoughTransformationRefactor

--- DOTest/AppDelegate.m
+++ DOTest/AppDelegate.m
@@ -9,6 +9,8 @@
 #import "AppDelegate.h"
 #import "HoughTransformator.h"
 #import "Circle.h"
+#import "CPUUsage.h"
+#import "InformantProtocol.h"
 
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
@@ -23,63 +25,69 @@
     [super dealloc];
 }
 	
-- (IplImage*)drawCircles:(NSMutableArray*) circles on:(IplImage*) img {
-    for (Circle* circle in circles) {
-        cvCircle(img, cvPoint(circle.x, circle.y), circle.r, CV_RGB(255,0,0), 3, 8, 0);
+- (void)worker:(id)host{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    NSString* currentHost = [[NSHost currentHost] localizedName];
+    
+    NSSocketPort *port = [[NSSocketPort alloc] init];
+    NSConnection *connection = [NSConnection connectionWithReceivePort:port sendPort:port];
+    BOOL isConnected = [[NSSocketPortNameServer sharedInstance] registerPort:port name:currentHost];
+    
+    
+    NSLog(@"I am %@.", currentHost);
+    
+    HoughTransformator * hough = [[HoughTransformator alloc] init];
+    [connection setRootObject: hough];
+    
+    if (!isConnected) {
+        NSLog(@"Impossible to vend this object.");
+    } else {
+        NSLog(@"Object vended.");
     }
-    return img;
+    [[NSRunLoop currentRunLoop] run];
+    
+    [pool release];
 }
 
-- (double)getFps:(time_t)end i:(int *)i start:(time_t)start
-{
-    time(&end);   
-    return ++(*i) / (difftime (end, start));
+- (void)informant:(id)host{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    NSString* currentHost = [[NSHost currentHost] localizedName];
+    NSString * serviceHost = [currentHost stringByAppendingString:@"_service"];
+    
+    NSSocketPort *port = [[NSSocketPort alloc] init];
+    NSConnection *connection = [NSConnection connectionWithReceivePort:port sendPort:port];
+    BOOL isConnected = [[NSSocketPortNameServer sharedInstance] registerPort:port name:serviceHost];
+    
+    NSLog(@"I am %@.", serviceHost);
+    
+    CPUUsage * informant = [[CPUUsage alloc] init];
+    [connection setRootObject: informant];
+    
+    
+    if (!isConnected) {
+        NSLog(@"Impossible to vend this object.");
+    } else {
+        NSLog(@"Object vended.");
+    }
+    [[NSRunLoop currentRunLoop] run];
+    
+    [pool release];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // ServiceUser usage, distributed object
-    HoughTransformator* hough = [[HoughTransformator alloc] init];
-    NSMutableArray* circles = [[NSMutableArray alloc] init];
+    NSLog(@"applicationDidFinishLaunching");
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    IplImage* img = NULL;
-    CvCapture *capture = 0;
-    capture = cvCaptureFromCAM(-1);
-    if (!capture) {
-        NSLog(@"Cannot initialize webcam");
-    }
-    CvFont* font;
-	cvInitFont(font,CV_FONT_HERSHEY_DUPLEX,1,0.8,0.2,1,8);
+    NSThread* worker = [[NSThread alloc] initWithTarget:self selector:@selector(worker:) object:nil];
+    NSThread* informant = [[NSThread alloc] initWithTarget:self selector:@selector(informant:) object:nil];
     
-    cvNamedWindow("result", CV_WINDOW_AUTOSIZE);
+    [worker start];
+    [informant start];
     
-    time_t start, end;
-    double fps;
-    int i = 0;
-    
-    time(&start);
-    
-    while (true) {
-        @autoreleasepool {
-            img = cvQueryFrame(capture);
-            if (!img) {
-                NSLog(@"Failed to retrive frame");
-            }
-            if (i % 2 == 0) {
-                circles = [hough performHoughTransformationWithIplImage:img];
-                img = [self drawCircles:circles on:img];
-            }
-            
-            fps = [self getFps:end i:&i start:start];
-            char c=cvWaitKey(33);
-            if(c==27)
-                break;
-            
-            cvPutText(img, [[NSString stringWithFormat:@"%.2lf", fps] UTF8String], cvPoint(30,30), font, cvScalarAll(255));
-            cvShowImage("result", img);
-        }
-    }
-
+    [pool drain];
 }
 
 @end
